c4ede1a502a88c73c08f059aad920a0f3a1701b3
yuutokawakami <y.uto.to06@gmail.com>
2018-03-10 15:30:16 +0900

    オークションアプリ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/items' => 'items#index' #追加
-  get "/items/:id" => "items#show"
+  #get '/items' => 'items#index'
+  #post '/items' => 'items#create'
+  #get '/items/new' => 'items#new'
+  #get '/items/:id' => 'items#show'
+  #get '/items/:id/edit' => 'items#edit'
+  #p3atch '/items/:id' => 'items#update'
+  #delete '/items/:id' => 'items#destroy' #追加
 
+ resources :items
 end
